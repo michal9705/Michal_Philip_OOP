5bf677c100848d4fedb0d32ff5138a95e018b121
Michal-Philip <michalp9705@gmail.com>
2026-01-03 20:34:01 +0200

    Add Q4 project files

---
+++ Ex.4/q3/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+class OperationTable<T>
+{
+    // delegate לפעולה בינארית
+    public delegate T OpFunc(T x, T y);
+
+    private List<T> row_values;
+    private List<T> col_values;
+    private T[,] table;
+    private OpFunc op;
+
+    public OperationTable(List<T> row_values, List<T> col_values, OpFunc op)
+    {
+        this.row_values = row_values;
+        this.col_values = col_values;
+        this.op = op;
+
+        table = new T[row_values.Count, col_values.Count];
+
+        // לולאה בתוך לולאה לחישוב הטבלה
+        for (int i = 0; i < row_values.Count; i++)
+        {
+            for (int j = 0; j < col_values.Count; j++)
+            {
+                table[i, j] = op(row_values[i], col_values[j]);
+            }
+        }
+    }
+
+    public void Print()
+    {
+        for (int i = 0; i < row_values.Count; i++)
+        {
+            for (int j = 0; j < col_values.Count; j++)
+            {
+                Console.Write($"{table[i, j]}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<double> row_values = new List<double>();
+        for (int i = 1; i <= 4; i++)
+        {
+            row_values.Add(1.0 / i);
+        }
+
+        List<double> col_values = new List<double>();
+        for (int i = 1; i < 5; i++)
+        {
+            col_values.Add(1.0 / i);
+        }
+
+        OperationTable<double> t1 =
+            new OperationTable<double>(row_values, col_values, (x, y) => x + y);
+
+        t1.Print();
+    }
+}
---
+++ Ex.4/q5/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+class Fraction
+{
+    private int numerator;
+    private int denominator;
+
+    // Constructor + צמצום השבר
+    public Fraction(int num, int den)
+    {
+        numerator = num;
+        denominator = den;
+        Reduce();
+    }
+
+    private int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return Math.Abs(a);
+    }
+
+    private void Reduce()
+    {
+        int gcd = GCD(numerator, denominator);
+        numerator /= gcd;
+        denominator /= gcd;
+    }
+
+    // Operator Overloading לחיבור
+    public static Fraction operator +(Fraction f1, Fraction f2)
+    {
+        return new Fraction(f1.numerator * f2.denominator + f2.numerator * f1.denominator,
+                            f1.denominator * f2.denominator);
+    }
+
+    public override string ToString()
+    {
+        return $"{numerator}/{denominator}";
+    }
+}
+
+class GenericOperationTable<T>
+{
+    private T[] elements;
+    private Func<T, T, T> operation; // delegate לפעולה
+
+    public GenericOperationTable(T[] elems, Func<T, T, T> op)
+    {
+        elements = elems;
+        operation = op;
+    }
+
+    public void PrintTable()
+    {
+        int n = elements.Length;
+
+        // הדפסת כותרות עמודות
+        Console.Write("       ");
+        foreach (var e in elements)
+            Console.Write($"{e,-8}");
+        Console.WriteLine();
+
+        // הדפסת השורות עם הפעולה
+        foreach (var row in elements)
+        {
+            Console.Write($"{row,-7}");
+            foreach (var col in elements)
+            {
+                T result = operation(row, col);
+                Console.Write($"{result,-8}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // יצירת השברים 1/12 עד 12/12
+        Fraction[] fractions = new Fraction[12];
+        for (int i = 1; i <= 12; i++)
+        {
+            fractions[i - 1] = new Fraction(i, 12);
+        }
+
+        // יצירת הטבלה הגנרית עם פעולת חיבור
+        GenericOperationTable<Fraction> table = new GenericOperationTable<Fraction>(
+            fractions,
+            (a, b) => a + b
+        );
+
+        // הדפסת לוח החיבור
+        table.PrintTable();
+    }
+}
